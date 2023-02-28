646cbad4e62c509f1012f19b8914404c13575e19
Carlos Rocamora <cakorocamora@gmail.com>
2023-02-28 21:51:38 +0100

    contracts added

---
+++ orion-priv-network/contract/keys.js
@@ -0,0 +1,39 @@
+module.exports = {
+  chainID: 1337,
+  orion: {
+    node1: {
+      publicKey: "JgnJvPpsuvk8Mgp4V/P1p0pMltDYdEMU9hk60BL9wkQ=",
+    },
+    node2: {
+      publicKey: "nNbBs8ggFDn1UYniunx/rni37b/kKvgsjDQ2pQEz/g4=",
+    },
+    node3: {
+      publicKey: "vF37Z3VQxKOcK3ZUoVzmE6xRcw4WgzmWTDdJObG/WDA=",
+    },
+    node4: {
+      publicKey: "mYT7BJY+Z2Qz1ZFTpov4pTL0XWuL6Ji/Iil+k2xVTGI=",
+    },
+  },
+  besu: {
+    node1: {
+      url: "http://localhost:8545",
+      privateKey:
+        "560786ab532f2660f4e64b6c2a10fa8b088b154871884dcab76cc5cce8735e2c",
+    },
+    node2: {
+      url: "http://localhost:8546",
+      privateKey:
+        "b95ac34ef7a56ccb8ffab94eaf97c3979cdeddd6fdf9d72c1278de97ed2f7a87",
+    },
+    node3: {
+      url: "http://localhost:8547",
+      privateKey:
+        "eb1f9a4362f3c50c3a413eb1f7efcc0e37ce3b04b8d7b6919bac00e9de00e8bb",
+    },
+    node4: {
+      url: "http://localhost:8548",
+      privateKey:
+        "1195c429d8498c1e8447f9c976dfbfc646a264949b0dc2cf8b06496cb60db846",
+    },
+  },
+};
