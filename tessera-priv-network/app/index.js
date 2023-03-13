643cdc9019f4ae40eea9e33f8a95ea062aecebcb
carlos <c.rocamora@complynow.eu>
2023-03-13 19:38:09 +0100

    update api and smart contracts imported

--- tessera-priv-network/app/index.js
+++ tessera-priv-network/app/index.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyparser = require("body-parser");
 app.use(bodyparser.json())
 const port = 3000;
+const Web3 = require('web3');
+const Web3Quorum = require('web3js-quorum');
+const { tessera, besu } = require("../contracts/keys.js");
+const { createContract, getValueAtAddress } = require("../contracts/private_tx.js");
 
 
 app.get("/", (req, res) => {
@@ -14,5 +18,13 @@ app.listen(port, () => {
 });
 
 app.post("/registerEvent",(req,res)=>{
+  console.log(req.body)
+})
+
+app.post("/deploy",(req,res)=>{
+  console.log(req.body)
+})
+
+app.post("/store",(req,res)=>{
   console.log(req.body)
 })
---
+++ tessera-priv-network/contracts/RouteFactory.sol
@@ -0,0 +1,121 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "./RouteContract.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+// Compile: solcjs --bin --abi --include-path node_modules/ --base-path . RouteFactory.sol
+
+contract RouteFactory {
+    using SafeMath for uint256;
+
+    enum RouteNames {
+        AP7,
+        M40,
+        CITY
+    }
+
+    enum RouteLocations {
+        ALICANTE,
+        ELCHE
+    }
+
+    address private immutable i_admin;
+    uint256 private s_nextRouteId;
+    uint256 private s_routeCounter;
+    RouteContract[] private routes_addresses;
+    mapping(string => mapping(string => uint256)) private s_routes_ids;
+
+    event routeDeleted(uint256 _deletedId);
+
+    //set the constructor
+    constructor() {
+        s_nextRouteId = 1;
+        s_routeCounter = 0;
+        routes_addresses = new RouteContract[](0);
+        i_admin = msg.sender;
+    }
+
+    //create a modifier that checks if the route already exists
+    modifier approveRouteCreation(
+        string memory _location,
+        string memory _name
+    ) {
+        require(s_routes_ids[_location][_name] == 0, "Route already exists");
+        _;
+    }
+
+    modifier routeExists(string memory _location, string memory _name) {
+        require(s_routes_ids[_location][_name] != 0, "Route doesn't exists");
+        _;
+    }
+
+    modifier isAdmin() {
+        require(i_admin == msg.sender, "Not allowed to create routes");
+        _;
+    }
+
+    function createRoute(
+        string memory _location,
+        string memory _name
+    ) public isAdmin approveRouteCreation(_location, _name) {
+        RouteContract route = new RouteContract(
+            s_nextRouteId,
+            _location,
+            _name
+        );
+        s_routes_ids[_location][_name] = s_nextRouteId;
+        routes_addresses.push(route);
+        s_nextRouteId = s_nextRouteId.add(1);
+        s_routeCounter = s_routeCounter.add(1);
+    }
+
+    function enterRoute(uint256 _routeIndex) public {
+        RouteContract(address(routes_addresses[_routeIndex])).enterWhiteList(
+            msg.sender
+        );
+    }
+
+    function leaveRoute(uint256 _routeIndex) public {
+        RouteContract(address(routes_addresses[_routeIndex])).leaveWhiteList(
+            msg.sender
+        );
+    }
+
+    function getRouteCars(
+        uint256 _routeIndex
+    ) public view returns (address[] memory) {
+        return
+            RouteContract(address(routes_addresses[_routeIndex]))
+                .getWhiteListedCars();
+    }
+
+    function getRouteCarsNumber(
+        uint256 _routeIndex
+    ) public view returns (uint256) {
+        return
+            RouteContract(address(routes_addresses[_routeIndex]))
+                .getWhiteListedCarsNumber();
+    }
+
+    function getRouteName(
+        uint256 _routeIndex
+    ) public view returns (string memory) {
+        return RouteContract(routes_addresses[_routeIndex]).getName();
+    }
+
+    function getRouteIndex(
+        string memory _location,
+        string memory _name
+    ) public view returns (uint256) {
+        return s_routes_ids[_location][_name] - 1;
+    }
+
+    function getNextId() public view returns (uint256) {
+        return s_nextRouteId;
+    }
+
+    function getNumberOfRoutes() public view returns (uint256) {
+        return s_routeCounter;
+    }
+}
